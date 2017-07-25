a5823f7264fe304e0c3b27be092f44e3e6c21e2f
Michael Fester <michael.fester@gmail.com>
2017-07-25 11:39:57 +0200

    Update tests

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from setuptools import setup
 
 setup(
     name='snipsfakeweather',
-    version='0.1.2',
+    version='0.1.3',
     description='Fake weather forescasts skill for Snips',
     author='Michael Fester',
     author_email='michael.fester@gmail.com',
     url='https://github.com/snipsco/snips-skill-fakeweather',
     download_url='',
     license='MIT',
+    install_requires=['enum'],
     test_suite="tests",
     keywords=['snips'],
     packages=[
